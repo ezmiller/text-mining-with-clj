a18615a2d6c2783aaa2b0df9ae8d2e92c6aba73e
Ethan Miller <ethanzanemiller@gmail.com>
2020-04-04 21:05:55 -0400

    Upgrade notespace

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [scicloj/clojisr "1.0.0-BETA10"]
-                 [scicloj/notespace "2.0.0-alpha3"]]
+                 [scicloj/notespace "2.0.0-alpha4"]]
   :main ^:skip-aot text-mining-with-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
