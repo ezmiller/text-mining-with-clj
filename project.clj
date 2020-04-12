c651f4fdf6df626ed0cb5bcf80df1cc7f02da74d
Ethan Miller <ethanzanemiller@gmail.com>
2020-04-12 10:23:44 -0400

    Upgrade clojisr

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [scicloj/clojisr "1.0.0-BETA10"]
+                 [scicloj/clojisr "1.0.0-BETA11-SNAPSHOT"]
                  [scicloj/notespace "2.0.0-alpha4"]]
   :main ^:skip-aot text-mining-with-clj.core
   :target-path "target/%s"
